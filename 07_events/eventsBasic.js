fb8c6606268d0bb8486fad424a5d34666f9b9407
KirHash <104854308+KirHash@users.noreply.github.com>
2024-08-24 21:44:51 +0530

    Add files via upload

---
+++ 07_events/eventsBasic.js
@@ -0,0 +1,5 @@
+// All the code in JS runs sequentially
+// But, the events are invoked on an activity
+// Events Listener provides the ability of propagation ability
+// Bubbling: Where the activity bubbles up. This means that the most inner event invokes first (when false)
+// Capturing: In this the outermost activity invokes first (when true)
